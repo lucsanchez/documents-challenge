f7e71ebf98d9b8ddccc06a24a9d8d30d01938a98
lucsanchez <luc.sanchez@hotmail.com>
2025-01-16 19:37:58 +0100

    fix: styles

--- frontend/src/ui/components/documentCard/documentCard.tsx
+++ frontend/src/ui/components/documentCard/documentCard.tsx
@@ -5,6 +5,7 @@ import styles from "./documentCard.module.scss";
 
 import clsx from "clsx";
 import { formatDateToDaysAgo } from "@/ui/utils/dateUtils";
+import React from "react";
 
 interface DocumentCardProps {
   document: Document;
@@ -13,23 +14,25 @@ interface DocumentCardProps {
 export function DocumentCard({ document }: DocumentCardProps) {
   const { isGridView } = useViewContext();
 
-  const cardClasses = clsx(styles.card, { [styles.column]: isGridView });
+  const cardClasses = clsx(styles.mainContent, { [styles.column]: isGridView });
   return (
-    <div className={cardClasses}>
-      <div className={styles.container}>
-        <h3>{document.title}</h3>
-        <p>Version {document.version}</p>
-      </div>
-      <div className={styles.container}>
-        {document.contributors?.map((contributor) => (
-          <p key={`${document.id} ${contributor.id}`}>{contributor.name}</p>
-        ))}
-      </div>
+    <div className={styles.card}>
+      <div className={cardClasses}>
+        <div className={styles.container}>
+          <h3>{document.title}</h3>
+          <p>Version {document.version}</p>
+        </div>
+        <div className={styles.container}>
+          {document.contributors?.map((contributor) => (
+            <p key={`${document.id} ${contributor.id}`}>{contributor.name}</p>
+          ))}
+        </div>
 
-      <div className={styles.container}>
-        {document.attachments?.map((attachment) => (
-          <p key={attachment}>{attachment}</p>
-        ))}
+        <div className={styles.container}>
+          {document.attachments?.map((attachment) => (
+            <p key={attachment}>{attachment}</p>
+          ))}
+        </div>
       </div>
       <p className={styles.extraInformation}>
         Updated: {formatDateToDaysAgo(document.updated_at)}
